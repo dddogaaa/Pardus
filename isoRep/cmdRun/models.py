c320299e716fb806b146c7d445478911bdcc1e31
dddogaaa <fatmadoga@hotmail.com>
2023-06-20 13:17:03 +0300

    cmdRun app added. (db not working)

---
+++ isoRep/cmdRun/models.py
@@ -0,0 +1,39 @@
+# Database import
+from django.db import models
+from django import db
+
+# Settings import
+from django.conf import settings
+
+# Other import
+from datetime import datetime
+
+
+def command_run_db(name, command, status,log_file):
+    date = datetime.now()
+
+    items = {
+        "Sdate": date,
+        "Fdate": None,
+        "File": log_file,
+        "Name": name,
+        "Command": command,
+        "Status": None
+    }
+
+    process_id = db.new_task(command)
+
+    if status == 0:
+        db.update_task(process_id, "Sdate", items["Sdate"])
+        db.update_task(process_id, "Fdate", datetime.now())
+        db.update_task(process_id, "File", items["File"])
+        db.update_task(process_id, "Name", items["Name"])
+        db.update_task(process_id, "Command", items["Command"])
+        db.update_task(process_id, "Status", status)
+    else:
+        db.update_task(process_id, "Sdate", items["Sdate"])
+        db.update_task(process_id, "Fdate", datetime.now())
+        db.update_task(process_id, "File", items["File"])
+        db.update_task(process_id, "Name", items["Name"])
+        db.update_task(process_id, "Command", items["Command"])
+        db.update_task(process_id, "Status", status)
---
+++ isoRep/cmdRun/views.py
@@ -0,0 +1,59 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import os
+import subprocess
+import datetime
+from django.conf import settings
+
+def run_command(command):
+    creationTime = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    log_dir = 'logs'
+    if not os.path.exists(log_dir):
+        os.makedirs(log_dir)
+
+    log_file = os.path.join(log_dir, f'{creationTime}_{command.replace(" ", "_")}.log')
+
+    process = subprocess.run(
+        command,
+        shell=True,
+        capture_output=True,
+        text=True
+    )
+
+    with open(log_file, 'w') as file:
+        file.write(process.stdout)
+        file.write(process.stderr)
+
+    status = process.returncode
+
+    return status, log_file
+
+def execute_command(request):
+    if request.method == 'POST':
+        command = request.POST.get('command')
+        # name eklenicek
+        status, log_file = run_command(command)
+        log_content = ""
+
+        if os.path.isfile(log_file):
+            with open(log_file, 'r') as file:
+                log_content = file.read()
+
+        return render(request, 'result.html', {'command': command, 'log_file': log_file, 'log_content': log_content, 'status': status})
+
+    return render(request, 'index.html')
+
+def serve_log_file(request, log_file_name):
+    try:
+        with open(log_file_name, 'r') as file:
+            file_content = file.read()
+            
+            # If you want the file downloadable:
+            # response = HttpResponse(file_content, content_type='text/plain')
+            # response['Content-Disposition'] = f'attachment; filename={log_file_name}'
+            # return response
+            
+            return HttpResponse(file_content, content_type='text/plain')
+
+    except FileNotFoundError:
+        return HttpResponse(f'Log file {log_file_name} not found.', status=404)
